91a1dede0876f798b6f2f84648d94cf075c38c48
= <=>
2016-12-16 13:18:10 +1100

    Fixed bugs that led to false positives and false negatives in mod searches

--- src/bundler.py
+++ src/bundler.py
@@ -25,32 +25,33 @@ INDENT_STRING = '    '
 
 #this is just a stub for now, needs TOML support to do properly, also the extern may not actually be mandatory in rust, need to check
 def processLocalCrateUse(line, curIndent = ''):
-    # crateUse = re.search('^extern crate \w+;', line);
-    # if crateUse is not None:
-    # 	cwd = os.getcwd()
-    # 	modName = crateUse.group(0).split('extern crate ')[1].split(';')[0]
-    #
-    # 	cratePath = localCratePaths[modName]
-    # 	os.chdir('../%s/src' % cratePath)
-    #
-    # 	# print(modName, os.getcwd())
-    #
-    # 	with open('./lib.rs', 'r') as libFile:
-    # 		# for line in libFile.readlines():
-    # 		# 	print(line)
-    # 		modContents = scan(libFile, curIndent+'\t')
-    #
-    # 	result += "%s%s mod %s {\n%s\n%s}" % (curIndent, 'pub', modName, modContents, curIndent)
-    #
-    # 	os.chdir(cwd)
-    return None
+    localCratePaths = {'phys2d': '/mnt/shared-drive/archy/git/coding-game/common/phys2d', 'cg_generic_traits': '/mnt/shared-drive/archy/git/coding-game/common/cg_generic_traits'}
+    crateUse = re.search('^(\s*)?extern crate \w+;', line);
+    result = ''
+    if crateUse is not None:
+        cwd = os.getcwd()
+        modName = crateUse.group(0).split('extern crate ')[1].split(';')[0]
+    
+        cratePath = localCratePaths[modName]
+        os.chdir('%s/src' % cratePath)
+        with open('./lib.rs', 'r') as libFile:
+            # for line in libFile.readlines():
+            #     print(line)
+            modContents = scan(libFile, curIndent+'\t')
+    
+        result += "%s%s mod %s {\n%s\n%s}" % (curIndent, 'pub', modName, modContents, curIndent)
+    
+        os.chdir(cwd)
+        return result
+    else:
+        return None
 
 def processMod(line, curIndent = ''):
     mod = re.search('^(\s*)?(pub )?mod \w+;', line)
     if mod is not None:
         publicity, modName = mod.group(0).split('mod ')
         publicity = publicity.strip()
-        modName = modName.split(';')[0]
+        modName = modName.split(';')[0].strip()
         modContents = ''
         if os.path.isfile('./%s.rs' % modName):
             with open('./%s.rs' % modName, 'r') as subFile:
@@ -59,8 +60,8 @@ def processMod(line, curIndent = ''):
             os.chdir('./%s' % modName);
             with open('./mod.rs', 'r') as subFile:
                 modContents = scan(subFile, curIndent+INDENT_STRING)
-                os.chdir('..')
-            return "%s%s mod %s {\n%s\n%s}" % (curIndent, publicity, modName, modContents, curIndent)
+            os.chdir('..')
+        return "%s%s mod %s {\n%s\n%s}" % (curIndent, publicity, modName, modContents, curIndent)
     else:
         return None
 
@@ -76,7 +77,7 @@ def scan(file, curIndent = ''):
         temp = processMod(line, curIndent)
         if temp is not None:
             result += curIndent + temp
-
+            continue
         result += curIndent + line
     return result
 
